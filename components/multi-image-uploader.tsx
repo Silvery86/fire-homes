4b5826d4b32cc123658fb927a88c8f8a3441a2a5
giang <giang8692@gmail.com>
2025-09-09 21:58:43 +0700

    #23 Add Images upload previews

--- components/multi-image-uploader.tsx
+++ components/multi-image-uploader.tsx
@@ -2,6 +2,10 @@
 
 import { useRef } from "react";
 import { Button } from "./ui/button";
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
+import Image from "next/image";
+import { Badge } from "./ui/badge";
+import { HandIcon, MoveIcon, XIcon } from "lucide-react";
 
 export type ImageUpload = {
     id: string;
@@ -16,14 +20,14 @@ type Props = {
 }
 
 export default function MultiImageUploader(
-    { 
+    {
         images = [],
         onImagesChange
-    } 
-    : Props
+    }
+        : Props
 ) {
     const uploadInputRef = useRef<HTMLInputElement | null>(null);
-    console.log({images});
+    console.log({ images });
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const files = Array.from(event.target.files || []);
         const newImages = files.map((file, index) => {
@@ -36,9 +40,53 @@ export default function MultiImageUploader(
         onImagesChange([...images, ...newImages]);
     }
     return (<div className="w-full max-w-3xl mx-auto p-4">
-        <input hidden ref={uploadInputRef} type="file" multiple accept="image/*" onChange={handleFileChange}/>
-        <Button type="button" onClick={() => uploadInputRef.current?.click()}>
+        <input hidden ref={uploadInputRef} type="file" multiple accept="image/*" onChange={handleFileChange} />
+        <Button className="w-full" variant="outline" type="button" onClick={() => uploadInputRef.current?.click()}>
             Upload Images
         </Button>
+        <DragDropContext onDragEnd={() => { }}>
+            <Droppable droppableId="property-images" direction="vertical">
+                {(provided) => (
+                    <div {...provided.droppableProps} ref={provided.innerRef} >
+                        {images.map((image, index) => (
+                            <Draggable key={image.id} draggableId={image.id} index={index}>
+                                {(provided) => (
+                                    <div
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        className="relative p-2"
+                                    >
+                                        <div className="bg-gray-200 rounded-2xl flex items-center gap-2">
+                                            <div className="size-40 relative rounded-2xl overflow-hidden">
+                                                <Image src={image.url} alt={`Property Image ${index + 1}`} className="object-cover" fill />
+                                            </div>
+                                            <div className="flex-grow">
+                                                <p className="text-md font-medium">{image.file?.name || `Image ${index + 1}`}</p>
+                                                {
+                                                    index === 0 && (
+                                                        <Badge variant="primary">
+                                                            Feature Image
+                                                        </Badge>
+                                                    )
+                                                }
+                                            </div>
+                                            <div className="flex items-center">
+                                                <button className="cursor-pointer p-2">
+                                                    <XIcon className="text-destructive" />
+                                                </button>
+                                                <div>
+                                                    <MoveIcon />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
+                            </Draggable>
+                        ))}
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
     </div>)
 }
